9232de765f3a6fe80c1bd4b9440e84fce2e2dae5
levg34 <guilhem.dross@laposte.net>
2021-02-08 11:16:10 +0100

    continue experimenting with nextjs

--- pages/database.js
+++ pages/database.js
@@ -4,12 +4,15 @@ import axios from 'axios'
 
 export default function Database() {
     const [luc, setLuc] = useState('')
+    const [lucbd, setLucBD] = useState()
 
     function handleSubmit(e) {
         e.preventDefault()
-        axios.post('/api/save',{luc,date:(new Date()).toISOString()})
+        let post = axios.post('/api/save',{luc,date:(new Date()).toISOString()})
+        post.then(value => {
+            setLucBD(value.data)
+        })
         setLuc('')
-        window.location = '/lucs'
     }
 
     return (<>
@@ -18,6 +21,7 @@ export default function Database() {
             <input type="text" name="luc" onChange={e => setLuc(e.target.value)} value={luc}></input>
             <button type="submit">Valider</button>
         </form>
+        {lucbd && <Link href={'/luc/'+lucbd._id}><pre>{JSON.stringify(lucbd, null, 2)}</pre></Link>}
         <Link href="/">Retour maison</Link>
     </>)
 }
